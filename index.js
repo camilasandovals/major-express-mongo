fec06a37eb5341dcf671f1ae7556451de286a09b
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-07 15:26:04 -0400

    built API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
+import { addClothing, getClothing, deleteClothing } from "./src/clothing.js";
 
+//
 
 const PORT = 3000;
 const app = express ()
@@ -8,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 //
+
+app.post("/clothes", addClothing);
+app.get("/clothes", getClothing);
+app.delete("/clothes/:docId", deleteClothing)
 
 app.listen(PORT, ()=> {
     console.log(`Listening to http://localhost:${PORT}..`)
