e2985361b15bdba87912f6318f7f391943b4c0b1
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-07 15:30:01 -0400

    ADD create function

--- src/clothing.js
+++ src/clothing.js
@@ -1,3 +1,11 @@
 import { db } from './dbConnect.js'
 import { ObjectId } from 'mongodb'
 const coll = db.collection('clothing')
+
+//Create
+
+export async function addClothing(req, res) {
+    const newClothes = req.body
+    await coll.insertOne(newClothes)
+    res.status(201).send({message: "new cloth added"})
+}
